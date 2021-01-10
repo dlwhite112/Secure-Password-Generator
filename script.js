911fcdda09ace1f41ee19f874cd5a2fe03e4a419
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-01-10 10:42:00 -0600

    add initial button function & define variables

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//prompts for lenght, upper, lower, #, special
 
-  passwordText.value = password;
+// // display password
 
-}
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", runGenerator);
+
+function runGenerator() {
+
+  alert ("Time to create your password!");
+
+  //password characteristics
+  var lenth = prompt("Choose Password Length *must be between 8 - 128 characters*")
+  var up = confirm("Include Uppercase characters?")
+  var low = confirm("Include Lowercase characters?")
+  var num = confirm("Include Numerical characters?")
+  var spec = confirm("Include Special characters?")
+}
